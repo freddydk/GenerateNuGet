cd63d0fbe0c532955947aa6dda16e2a31998f048
freddydk <freddydk@users.noreply.github.com>
2024-01-04 15:54:59 +0100

    containerhelper

--- HelperFunctions.ps1
+++ HelperFunctions.ps1
@@ -1,4 +1,4 @@
-$bcContainerHelperPath = 'https://github.com/freddydk/navcontainerhelper/archive/refs/heads/nuget.zip'
+$bcContainerHelperVersion = 'https://github.com/freddydk/navcontainerhelper/archive/refs/heads/nuget.zip'
 
 $tempName = Join-Path ([System.IO.Path]::GetTempPath()) ([Guid]::NewGuid().ToString())
 Write-Host "Downloading BcContainerHelper developer version from $bcContainerHelperVersion"
