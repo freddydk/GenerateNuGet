d8daaa9cb288eba6193505f181282cd2c5e9fe90
freddydk <freddydk@users.noreply.github.com>
2024-01-05 09:30:36 +0100

    test

--- DetermineArtifacts.ps1
+++ DetermineArtifacts.ps1
@@ -33,6 +33,9 @@ else {
     $artifactVersions = @(GetArtifactVersionsNeeded -apps $apps -allArtifactVersions $allArtifactVersions -runtimeDependencyPackageIds $runtimeDependencyPackageIds -nuGetServerUrl $nuGetServerUrl -nuGetToken $nuGetToken)
 }
 
+# Temporarily limit to 2 versions
+$artifactVersions = @($artifactVersions | Select-Object -Last 5 | Select-Object -First 2)
+
 $artifactVersions = @($artifactVersions | ForEach-Object { @{ "artifactVersion" = "$_"; "incompatibleArtifactVersion" = "$($_.Major).$($_.Minor+1)" } })
 
 
